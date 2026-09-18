41a737c84aeddfca3aefb8274eab834574f20369
AgustinRoss39 <104650789+AgustinRoss39@users.noreply.github.com>
2026-09-18 18:05:16 -0300

    Centraliza configuración de invitación Emilia Mernes

---
+++ js/config.js
@@ -0,0 +1,47 @@
+window.INVITATION_CONFIG = {
+  invitation: {
+    name: "Vicky",
+    ageLabel: "CUMPLE 8 AÑOS",
+    message: "Te invito a festejar mi cumple de 8 años al ritmo de Emilia Mernes. ¡Te espero!",
+    eventDateTime: "2026-10-04T15:00:00-03:00",
+    eventEndDateTime: "2026-10-04T17:30:00-03:00",
+    dateLabel: "Domingo 4 de Octubre",
+    timeLabel: "15:00 a 17:30 hs.",
+    venue: "Rapsodia Multieventos",
+    address: "Av. Roca 1797, Hurlingham",
+    mapUrl: "https://maps.app.goo.gl/vx6PLRWgpxs4wVMq9",
+    whatsappNumber: "5491156223007",
+    whatsappMessage: "¡Hola! Confirmo mi asistencia al cumpleaños de Vicky. 💖✨",
+    instagramUrl: "https://www.instagram.com/rossdigitalstudio/",
+    instagramHandle: "@rossdigitalstudio",
+    audioUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionEmiliaMernes/main/www.tarjetas.cool/modelos/cumple/emiliaMernes/sounds/emilia.mp3",
+    portraitUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionEmiliaMernes/main/www.tarjetas.cool/modelos/cumple/emiliaMernes/img/foto1.jpg",
+    ribbonUrl: "https://raw.githubusercontent.com/DigitalRoss/invitacionEmiliaMernes/main/www.tarjetas.cool/modelos/cumple/emiliaMernes/img/cinta.png"
+  },
+  characters: [
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionEmiliaMernes/main/www.tarjetas.cool/modelos/cumple/emiliaMernes/img/2.png",
+      width: "min(94vw, 430px)",
+      top: "22px",
+      translateX: "-50%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionEmiliaMernes/main/www.tarjetas.cool/modelos/cumple/emiliaMernes/img/1.png",
+      width: "min(90vw, 415px)",
+      top: "20px",
+      translateX: "-50%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionEmiliaMernes/main/www.tarjetas.cool/modelos/cumple/emiliaMernes/img/3.png",
+      width: "min(94vw, 430px)",
+      top: "24px",
+      translateX: "-50%"
+    },
+    {
+      src: "https://raw.githubusercontent.com/DigitalRoss/invitacionEmiliaMernes/main/www.tarjetas.cool/modelos/cumple/emiliaMernes/img/4.png",
+      width: "min(64vw, 290px)",
+      top: "28px",
+      translateX: "-50%"
+    }
+  ]
+};
